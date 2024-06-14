38d9c72ef3fe730af99e5b03cfacef884a2772e0
Ignasi <ignasi.lopez.luna@gmail.com>
2024-06-14 22:07:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# GenAI NodeJS
+
+This is an educational project that demonstrates how to use Large Language Models (LLMs) in NodeJS applications.
+The project contains subdirectories with examples of how to use LLMs in different scenarios.
+The LLMs will run locally inside [Ollama](https://ollama.com/) and Ollama will run in a [Docker](https://docker.com/)
+container managed by [Testcontainers](https://www.testcontainers.com/).
+
+## Table of Contents
+
+1. [Hello World](hello-world/README.md)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ hello-world/index.js
@@ -0,0 +1,27 @@
+const {GenericContainer} = require("testcontainers");
+const axios = require('axios');
+
+const main = async () => {
+    try {
+        console.log('Downloading Large Language Model, this may take a while...');
+        const container = await new GenericContainer("langchain4j/ollama-orca-mini")
+            .withExposedPorts(11434)
+            .start();
+
+        const url = `http://localhost:${container.getMappedPort(11434)}/api/generate`;
+        const data = {
+            model: "orca-mini",
+            prompt: "Hello world!",
+            stream: false
+        };
+
+        const response = await axios.post(url, data);
+        console.log(response.data.response);
+
+        await container.stop();
+    } catch (error) {
+        console.error('Error occurred:', error);
+    }
+};
+
+main();
