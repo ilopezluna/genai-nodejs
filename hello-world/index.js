a6e4122f0406184537af7b8e6662545d1532846c
Ignasi <ignasi.lopez.luna@gmail.com>
2024-06-23 10:17:52 +0200

    Using Ollama module of Testcontainers (#3)

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,14 +1,11 @@
-const {GenericContainer} = require("testcontainers");
 const axios = require('axios');
+const {OllamaContainer} = require("@testcontainers/ollama");
 
 const main = async () => {
     try {
         console.log('Downloading Large Language Model, this may take a while...');
-        const container = await new GenericContainer("langchain4j/ollama-orca-mini")
-            .withExposedPorts(11434)
-            .start();
-
-        const url = `http://localhost:${container.getMappedPort(11434)}/api/generate`;
+        const container = await new OllamaContainer("langchain4j/ollama-orca-mini").start();
+        const url = `${container.getEndpoint()}/api/generate`;
         const data = {
             model: "orca-mini",
             prompt: "Hello world!",
@@ -18,8 +15,6 @@ const main = async () => {
         console.log('Waiting for model answer, this may take a while depending on the model size and your hardware...');
         const response = await axios.post(url, data);
         console.log(`🤖${response.data.response}`);
-
-        await container.stop();
     } catch (error) {
         console.error('Error occurred:', error);
     }
--- vision-model/index.js
+++ vision-model/index.js
@@ -1,17 +1,15 @@
-const {GenericContainer} = require("testcontainers");
 const axios = require('axios');
 const {readFileSync} = require("node:fs");
+const {OllamaContainer} = require("@testcontainers/ollama");
 
 const main = async () => {
     try {
         console.log('Downloading Large Language Model, this may take a while...');
-        const container = await new GenericContainer("ollama/ollama")
-            .withExposedPorts(11434)
-            .start();
+        const container = await new OllamaContainer("langchain4j/ollama-orca-mini").start();
 
         await container.exec(['ollama', 'pull', 'moondream']);
         const base64Image = readFileSync('whale.jpeg', 'base64');
-        const url = `http://localhost:${container.getMappedPort(11434)}/api/generate`;
+        const url = `${container.getEndpoint()}/api/generate`;
         const data = {
             model: "moondream",
             prompt: "Describe the image",
@@ -22,8 +20,6 @@ const main = async () => {
         console.log('Waiting for model answer, this may take a while depending on the model size and your hardware...');
         const response = await axios.post(url, data);
         console.log(`${response.data.response}`);
-
-        await container.stop();
     } catch (error) {
         console.error('Error occurred:', error);
     }
