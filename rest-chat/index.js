d6f231302519253e1165ca2f5eb9440a94dacafe
Ignasi <ignasi.lopez.luna@gmail.com>
2024-06-23 10:16:52 +0200

    Using Ollama module of Testcontainers

--- rest-chat/index.js
+++ rest-chat/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const {GenericContainer} = require("testcontainers");
 const {post} = require("axios");
+const {OllamaContainer} = require("@testcontainers/ollama");
 const app = express()
 const port = 3000
 let container;
@@ -8,9 +8,7 @@ let container;
 const main = async () => {
     try {
         console.log('Downloading Large Language Model, this may take a while...');
-        container = await new GenericContainer("langchain4j/ollama-orca-mini")
-            .withExposedPorts(11434)
-            .start();
+        container = await new OllamaContainer("langchain4j/ollama-orca-mini").start();
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
             console.log(`Visit http://localhost:${port}/?prompt=Tell%20me%20something%20interesting`)
@@ -22,7 +20,7 @@ const main = async () => {
 main();
 
 app.get('/', async (req, res) => {
-    const url = `http://localhost:${container.getMappedPort(11434)}/api/generate`;
+    const url = `${container.getEndpoint()}/api/generate`;
     const data = {
         model: "orca-mini",
         prompt: req.query.prompt,
