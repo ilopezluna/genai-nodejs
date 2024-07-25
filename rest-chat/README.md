8fead638f022804f0f7c1e119ed708594a4557ba
Ignasi López <ignasi.lopez.luna@gmail.com>
2024-07-25 15:56:30 +0200

    Example of chatbot

--- rest-chat/README.md
+++ rest-chat/README.md
@@ -3,6 +3,6 @@
 This is a simple example of how to interact via REST with a chatbot model.
 
 The project contains a single NodeJS script, [index.js](index.js), that uses
-the [orca-mini](https://ollama.com/library/orca-mini) model to generate text based on a prompt.
+the [orca-mini](https://ollama.com/library/orca-mini) model show an example of how to interact with a chatbot model.
 The model runs locally inside [Ollama](https://ollama.com/) and Ollama runs in a container managed
 by [Testcontainers](https://www.testcontainers.com/).
--- rest-chat/index.js
+++ rest-chat/index.js
@@ -7,21 +7,25 @@ let container;
 
 const main = async () => {
     console.log('Downloading Large Language Model, this may take a while...');
-    container = await new OllamaContainer("langchain4j/ollama-orca-mini").start();
+    container = await new OllamaContainer("ilopezluna/ollama-orca-mini:0.2.8-3b").withReuse().start();
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
-        console.log(`Visit http://localhost:${port}/?prompt=Tell%20me%20something%20interesting`)
+        console.log(`Visit http://localhost:${port}/api/chat?prompt=${encodeURIComponent("Greetings!")}`)
     });
 };
 main();
 
-app.get('/', async (req, res) => {
-    const url = `${container.getEndpoint()}/api/generate`;
+app.get('/api/chat', async (req, res) => {
+    const url = `${container.getEndpoint()}/api/chat`;
     const data = {
-        model: "orca-mini",
-        prompt: req.query.prompt,
+        model: "orca-mini:3b",
+        messages: [
+            {role: "assistant", content: "What is your name?"},
+            {role: "user", content: "My name is Ignasi"},
+            {role: "user", content: req.query.prompt}
+        ],
         stream: false
     };
     const response = await post(url, data);
-    res.send(`Response from LLM (🤖)-> ${response.data.response}`)
+    res.send(`Response from LLM (🤖)-> ${response.data.message.content}`)
 });
